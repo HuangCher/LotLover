e43d66af8a15a22e770fdb8816c0efd1da6ce87f
ChasePrasad <chaseprasad1@gmail.com>
2025-02-28 22:46:24 -0500

    added mongo to backend

---
+++ server/routes/record.js
@@ -0,0 +1,76 @@
+import express from "express";
+import db from "../db/connection.js";
+import { ObjectId } from "mongodb";
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  let collection = await db.collection("records");
+  let results = await collection.find({}).toArray();
+  res.status(200).send(results);
+});
+
+router.get("/:id", async (req, res) => {
+  let collection = await db.collection("records");
+  let query = { _id: new ObjectId(req.params.id) };
+  let result = await collection.findOne(query);
+
+  if (!result) res.status(404).send("User not found");
+  else res.status(200).send(result);
+});
+
+router.post("/", async (req, res) => {
+  try {
+    let newDocument = {
+      username: req.body.username,
+      password: req.body.password,
+      pass_level: req.body.pass_level,
+      ufid: req.body.ufid,
+    };
+    let collection = await db.collection("records");
+    let result = await collection.insertOne(newDocument);
+
+    res.status(204).send(result);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error adding user");
+  }
+});
+
+router.patch("/:id", async (req, res) => {
+  try {
+    const query = { _id: new ObjectId(req.params.id) };
+    const updates = {
+      $set: {
+        username: req.body.username,
+        password: req.body.password,
+        pass_level: req.body.pass_level,
+        ufid: req.body.ufid,
+      },
+    };
+
+    let collection = await db.collection("records");
+    let result = await collection.updateOne(query, updates);
+
+    res.status(200).send(result);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error updating user");
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const query = { _id: new ObjectId(req.params.id) };
+    const collection = db.collection("records");
+
+    let result = await collection.deleteOne(query);
+
+    res.status(200).send(result);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error deleting user");
+  }
+});
+
+export default router;
