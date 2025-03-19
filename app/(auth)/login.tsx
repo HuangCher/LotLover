858d1cb2d5020b516d5b00088c8b531122c7e76c
ChasePrasad <chaseprasad1@gmail.com>
2025-03-19 01:10:16 -0400

    final expo fix (hopefully)

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -3,11 +3,6 @@ import React, { useState } from 'react';
 import { Stack } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useRouter } from "expo-router";
-<<<<<<< Updated upstream
-
-export default function LoginScreen() { 
-    const router = useRouter();
-=======
 import axios from "axios";
 
 export default function LoginScreen() { 
@@ -29,8 +24,6 @@ export default function LoginScreen() {
             console.log(err)
         });
     };
->>>>>>> Stashed changes
-
     return (
         <View >
             <Text style={{ fontSize: 24, marginBottom: 20 }}>Login</Text>
